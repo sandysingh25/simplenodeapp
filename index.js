96c8b7e9875ee86b3928fad66a3e3e2a4d222903
sandysingh25 <sandeepsgrover@gmail.com>
2020-08-09 07:54:58 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const server = http.createServer((req,res)=> {
+    if (req.url=== '/') {
+        res.end('<h1>Hello, World </ht>');
+    }
+});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log('Server running on port ${PORT}'));
